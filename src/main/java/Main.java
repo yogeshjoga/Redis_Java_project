8376b732508860bce19d8134198ae8dc03f6cf34
yogesh joga <36118169+yogeshjoga@users.noreply.github.com>
2024-11-29 22:16:24 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,8 @@ import java.net.Socket;
 
 public class Main {
   public static void main(String[] args){
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-//      Uncomment this block to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
@@ -30,8 +28,6 @@ public class Main {
             }
           } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
-
-            
           }
         }
   }
